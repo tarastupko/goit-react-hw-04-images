615e5b1992812354021fcb862ea182752a400969
tarastupko <109355475+tarastupko@users.noreply.github.com>
2023-08-20 19:41:30 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { AppContainer } from './App.styled';
 import Searchbar from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
